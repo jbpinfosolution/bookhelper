9d7fd7adc18dea830d7ab08a07c728357e26ddb6
jitendrasen_09 <jitendersen8959@gmail.com>
2022-10-25 20:49:52 +0530

    my first ionic project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
+
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
@@ -26,16 +28,16 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
+    {/* <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+        <Route exact path="/home"> */}
           <Home />
-        </Route>
+        {/* </Route>
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
       </IonRouterOutlet>
-    </IonReactRouter>
+    </IonReactRouter> */}
   </IonApp>
 );
 
